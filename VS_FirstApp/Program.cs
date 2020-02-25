a3ddff3962170429f2b8b273f24dd9c35c81e3f4
Jindal <akansha.jindal@siemens.com>
2020-02-25 10:00:36 +0530

    Add project files.

---
+++ VS_FirstApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_FirstApp
+{
+    class Program
+    {
+        //static int Main(string[] args)
+        //static int Main()
+        //static void Main()
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
